164ad2db1a0f3eac5243a6c46039f1ef607ee08f
Flavio Neubauer <flavio.neubauer@gmail.com>
2024-01-27 17:12:57 -0300

    mais testes de unidade no cliente e configuracao do h2

--- src/test/java/br/com/fiap/soat1/t32/ClienteStepsIntegrationTest.java
+++ src/test/java/br/com/fiap/soat1/t32/ClienteStepsIntegrationTest.java
@@ -1,48 +1,84 @@
 package br.com.fiap.soat1.t32;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.http.HttpStatusCode;
+
+import java.io.IOException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertTrue;
 
 public class ClienteStepsIntegrationTest extends SpringIntegrationTest {
 
-    private String body;
+	private String body;
+	private String cpf;
 
-    @Given("a valid body request to register a new client")
-    public void a_valid_body_request_to_register_a_new_client() {
-       this.body = "{\"nome\":\"João da Silva\",\"cpf\":\"41765533082\",\"email\":\"joao@example.com\"}";
-    }
+	@Given("a valid body request to register a new client")
+	public void a_valid_body_request_to_register_a_new_client() {
+		this.body = "{\"nome\":\"João da Silva\",\"cpf\":\"41765533082\",\"email\":\"joao@example.com\"}";
+	}
 
-    @Given("an invalid cpf to register a new client")
-    public void anInvalidCpfToRegisterANewClient() {
-        this.body = "{\"nome\":\"João da Silva\",\"cpf\":\"12345678900\",\"email\":\"joao@example.com\"}";
-    }
+	@Given("an invalid cpf to register a new client")
+	public void anInvalidCpfToRegisterANewClient() {
+		this.body = "{\"nome\":\"João da Silva\",\"cpf\":\"12345678900\",\"email\":\"joao@example.com\"}";
+	}
 
-    @When("the user call POST \\/v1\\/clientes")
-    public void theUserCallPOSTVClientes() throws IOException{
-        executePost("http://localhost:8080/v1/clientes",body);
-    }
-    
-    @Then("the client receives status code of {int}")
-    public void the_client_receives_status_code_of(Integer statusCode) throws IOException {
-        final HttpStatusCode currentStatusCode = latestResponse.getTheResponse().getStatusCode();
-        assertThat("status code is: " + latestResponse.getBody(), currentStatusCode.value(), is(statusCode));
-    }
+	@Given("an invalid name to register a new client")
+	public void anInvalidNameToRegisterANewClient() {
+		this.body = "{\"nome\":\"\",\"cpf\":\"41765533082\",\"email\":\"joao@example.com\"}";
+	}
 
-    @And("the request body has cpf invalido message")
-    public void theRequestBodyHasCpfInvalidoMessage() {
-        assertTrue(latestResponse.getBody().contains("CPF informado inválido"));
-    }
+	@Given("an invalid email to register a new client")
+	public void anInvalidEmailToRegisterANewClient() {
+		this.body = "{\"nome\":\"João da Silva\",\"cpf\":\"41765533082\",\"email\":\"@example.com\"}";
+	}
 
+	@Given("an existing cpf in the database")
+	public void anExistingCpfInTheDatabase() {
+		this.cpf = "41765533082";
+	}
 
+	@When("the user call POST \\/v1\\/clientes")
+	public void theUserCallPOSTVClientes() throws IOException {
+		executePost("http://localhost:8080/v1/clientes", body);
+	}
 
+	@When("the user call GET \\/v1\\/clientes\\/cpf")
+	public void theUserCallGETVClientesCpf() throws IOException {
+		executeGet("http://localhost:8080/v1/clientes/" + cpf);
+	}
+
+	@Then("the client receives status code of {int}")
+	public void the_client_receives_status_code_of(Integer statusCode) throws IOException {
+		final HttpStatusCode currentStatusCode = latestResponse.getTheResponse()
+				.getStatusCode();
+		assertThat("status code is: " + latestResponse.getBody(), currentStatusCode.value(), is(statusCode));
+	}
+
+	@And("the request body has cpf invalido message")
+	public void theRequestBodyHasCpfInvalidoMessage() {
+		assertTrue(latestResponse.getBody()
+				.contains("CPF informado inválido"));
+	}
+
+	@And("the request body has email invalido message")
+	public void theRequestBodyHasEmailInvalidoMessage() {
+		assertTrue(latestResponse.getBody()
+				.contains("E-mail informado inválido"));
+	}
+
+	@And("the request body has nome invalido message")
+	public void theRequestBodyHasNomeInvalidoMessage() {
+		assertTrue(latestResponse.getBody()
+				.contains("Nome informado inválido"));
+	}
+
+	@And("the request body has client data")
+	public void theRequestBodyHasClientData() {
+		assertTrue(latestResponse.getBody().contains(this.cpf));
+	}
 }
--- src/test/resources/vendas/cliente.feature
+++ src/test/resources/vendas/cliente.feature
@@ -8,3 +8,18 @@ Feature: the client api
     When the user call POST /v1/clientes
     Then the client receives status code of 422
     And the request body has cpf invalido message
+  Scenario: client send an invalid email
+    Given an invalid email to register a new client
+    When the user call POST /v1/clientes
+    Then the client receives status code of 422
+    And the request body has email invalido message
+  Scenario: client send an invalid name
+    Given an invalid name to register a new client
+    When the user call POST /v1/clientes
+    Then the client receives status code of 422
+    And the request body has nome invalido message
+  Scenario: client get object by cpf
+    Given an existing cpf in the database
+    When the user call GET /v1/clientes/cpf
+    Then the client receives status code of 200
+    And the request body has client data
